dd8e9145c0fd05b2f7c7bf7e8c5c3b812918de8b
Maxime PINEAU <maxime.pineau3@etu.univ-nantes.fr>
2016-07-14 15:24:56 +0530

    [code][gui] déplacement classe Application dans le package gui

---
+++ code/Robot/src/gui/Application.java
@@ -0,0 +1,13 @@
+package gui;
+import gui.controller.GeneralController;
+import gui.model.GeneralModel;
+
+
+public class Application {
+	public static void main(String[] args) {
+		
+		GeneralModel model = new GeneralModel();
+		new GeneralController(model);
+		
+	}
+}
